1f9a8a34ddc4616ff3413d4ec5e65d006dc4e638
heeone1 <rinakim1019@gmail.com>
2024-07-17 01:37:14 +0900

    초기설정

---
+++ src/test/java/com/example/TripChat/TripChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TripChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
